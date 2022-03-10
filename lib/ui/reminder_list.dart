193019e0078d2daa2a15cfdde0d311beaefdc203
brandonnhem <brandonnhem@gmail.com>
2022-03-09 17:18:25 -0800

    used new widget

--- lib/ui/reminder_list.dart
+++ lib/ui/reminder_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:reminder_app/widgets/reminder_card.dart';
+import 'package:reminder_app/model/Reminder.dart';
 
 class ReminderPage extends StatefulWidget {
   ReminderPage({Key? key}) : super(key: key);
@@ -9,15 +9,19 @@ class ReminderPage extends StatefulWidget {
 }
 
 class _ReminderPageState extends State<ReminderPage> {
+  List<Widget> cards = [
+    Reminder(time: '3:00 PM', description: 'Remember to feed the dogs!', imageUrl: 'assets/images/dogbowl.jpg'),
+    Reminder(time: 'Mar. 15', description: 'Pay Netflix bill', imageUrl: 'assets/images/netflixlogo.jpg'),
+    Reminder(time: '8:00 PM', description: 'Do laundry', imageUrl: 'assets/images/laundrybasket.jpg'),
+    Reminder(
+      time: '8:00 PM',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis, massa eu finibus convallis, massa turpis posuere dui, eget pellentesque augue urna vel dolor. Fusce tempor aliquet nullam.',
+      imageUrl: 'assets/images/trashbin.jpg'
+    )
+  ];
+  
   @override
   Widget build(BuildContext context) {
-    List<Widget> cards = [
-      reminderCard(context, 'assets/images/dogbowl.jpg', 'Feed the dogs', '2:00'),
-      reminderCard(context, 'assets/images/trashbin.jpg', 'Take out the ', '168:00'),
-      reminderCard(context, 'assets/images/toothbrush.jpg', 'Brush TEETH', '8:00'),
-      reminderCard(context, 'assets/images/laundrybasket.jpg', 'Do laundry', '5:00')
-    ];
-
     return Padding(
       padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
       child: ListView.builder(
