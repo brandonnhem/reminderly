490cc135e3f21052560cc7d81379c099f499e58d
brandonnhem <brandonnhem@gmail.com>
2022-03-10 12:30:50 -0800

    added network image example

--- lib/ui/reminder_list.dart
+++ lib/ui/reminder_list.dart
@@ -17,7 +17,11 @@ class _ReminderPageState extends State<ReminderPage> {
       time: '8:00 PM',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis, massa eu finibus convallis, massa turpis posuere dui, eget pellentesque augue urna vel dolor. Fusce tempor aliquet nullam.',
       imageUrl: 'assets/images/trashbin.jpg'
-    )
+    ),
+    Reminder(
+      time: '7:00 PM', 
+      description: 'Buy tacos',
+      networkImageUrl: 'https://images.squarespace-cdn.com/content/v1/528d18ace4b02c124321f3df/1566233965406-RUQMOKEDDA80E9DLPIP4/ke17ZwdGBToddI8pDm48kMXRibDYMhUiookWqwUxEZ97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0luUmcNM2NMBIHLdYyXL-Jww_XBra4mrrAHD6FMA3bNKOBm5vyMDUBjVQdcIrt03OQ/Street+Taco-1.jpg',)
   ];
   
   @override
